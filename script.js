402fcaf78031f4263c088158b6812770a6d1c86d
slogdesign <paucamcalderon@gmail.com>
2024-02-05 10:36:59 -0800

    sample

---
+++ script.js
@@ -0,0 +1,34 @@
+
+'use strict'
+
+const menuToggle = document.querySelector('.menu-toggle');
+const bxMenu = document.querySelector('.bx-menu');
+const bxX = document.querySelector('.bx-x');
+
+const navBar = document.querySelector('.navbar');
+const homeImgContainer = document.querySelector('.home-img-container');
+const homeImgWrapper = document.querySelector('.home-img-wrapper');
+// --- open menu ---
+
+bxMenu.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-menu')){
+        navBar.classList.add('show-navbar');
+        bxMenu.classList.add('hide-bx');
+        bxX.classList.add('show-bx');
+    }
+})
+
+// --- close menu ---
+
+bxX.addEventListener('click', (e)=> {
+    if(e.target.classList.contains('bx-x')){
+        navBar.classList.remove('show-navbar');
+        bxMenu.classList.remove('hide-bx');
+        bxX.classList.remove('show-bx');
+    }
+})
+homeImgContainer.addEventListener('mouseenter', () => {
+    homeImgWrapper.style.transform = 'rotateY(360deg)';
+  });
+  
+  
